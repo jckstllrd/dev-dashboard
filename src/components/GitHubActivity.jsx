198473436931c85768f0576197868d6eff88b8ca
jckstllrd <jackstallard17@gmail.com>
2026-02-21 11:10:27 +0000

    feat: add loading and error handling to github activity, refactor to remove getAcivity function

--- src/components/GitHubActivity.jsx
+++ src/components/GitHubActivity.jsx
@@ -6,33 +6,45 @@ const url = `https://api.github.com/repos/${username}/${repo}/commits`;
 
 export default function GitHubActivity() {
   const [activity, setActivity] = useState([]);
-  //   const [username, setUsername] = useState('jckstllrd');
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    const getActivityJson = async () => {
-      try {
-        let response = await fetch(url);
-        let result = await response.json();
-        setActivity(result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getActivityJson();
+    fetch(url)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setActivity(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
   }, []);
 
-  return (
-    <>
-      <h1>Your Github Activity</h1>
-      <ul>
-        {activity.map((commit) => (
-          <li key={commit.node_id}>
-            {commit.committer.login} -{' '}
-            <strong>"{commit.commit.message}"</strong>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <h1>Your Github Activity</h1>
+        <ul>
+          {activity.map((commit) => (
+            <li key={commit.node_id}>
+              {commit.committer.login} -{' '}
+              <strong>"{commit.commit.message}"</strong>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
